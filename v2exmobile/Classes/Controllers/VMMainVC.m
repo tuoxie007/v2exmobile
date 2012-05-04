f6d64e10d2caab74c3ff797288a1d5ee231318d7
Xu Ke <tuoxie007@gmail.com>
2012-05-05 02:09:33 +0800

    update view style 1. custom tabbar 2. custom navgation title 3. set status bar as black opaque

--- v2exmobile/Classes/Controllers/VMMainVC.m
+++ v2exmobile/Classes/Controllers/VMMainVC.m
@@ -19,20 +19,10 @@
 
 - (void)didReceiveMemoryWarning
 {
-    // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView
-{
-}
-*/
 
 - (void)viewDidLoad
 {
@@ -65,19 +55,19 @@
     
     [self setViewControllers:[[NSArray alloc] initWithObjects:timelineNavController, notificationNavController, nodeNavController, favoriteNavController, accountNavController, nil]];
     
+    [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"nav-title-bg.png"] forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.frame = CGRectMake(0, 0, 320, 39);
+    
     [super viewDidLoad];
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
