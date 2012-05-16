f9800b968cb1412aeca014d8f7cc8438e42d5a5f
Xu Ke <tuoxie007@gmail.com>
2012-05-17 02:51:52 +0800

    Patched InAppSettingsKit

---
+++ patch_dependence.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+patch dependence/InAppSettingsKit/InAppSettingsKit/Controllers/IASKAppSettingsViewController.m dependence/patchs/InAppSettingsKit/IASKAppSettingsViewController.m.diff
+patch dependence/InAppSettingsKit/InAppSettingsKit/Models/IASKSettingsReader.h dependence/patchs/InAppSettingsKit/IASKSettingsReader.h.diff
+patch dependence/InAppSettingsKit/InAppSettingsKit/Xibs/IASKPSTextFieldSpecifierViewCell.xib dependence/patchs/InAppSettingsKit/IASKPSTextFieldSpecifierViewCell.xib.diff
+patch dependence/InAppSettingsKit/InAppSettingsKit/Xibs/IASKPSToggleSwitchSpecifierViewCell.xib dependence/patchs/InAppSettingsKit/IASKPSToggleSwitchSpecifierViewCell.xib.diff
